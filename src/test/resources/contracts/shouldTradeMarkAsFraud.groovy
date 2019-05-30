0299f7b72392026377e1bd36f21a128917c6b55e
homeassist <home.assist.code@gmail.com>
2019-05-30 23:58:34 +0530

    adding more casees

--- src/test/resources/contracts/shouldTradeMarkAsFraud.groovy
+++ src/test/resources/contracts/shouldTradeMarkAsFraud.groovy
@@ -1,6 +1,8 @@
 package contracts
 
+[
 org.springframework.cloud.contract.spec.Contract.make {
+    name("Fraud Test")
     request {
         method 'GET'
         url '/fraudcheck?loanAmount=99999'
@@ -15,4 +17,23 @@ org.springframework.cloud.contract.spec.Contract.make {
             header('Content-Type': 'text/plain;charset=ISO-8859-1')
         }
     }
+},
+
+org.springframework.cloud.contract.spec.Contract.make {
+    name("Non-Fraud Test")
+    request {
+        method 'GET'
+        url '/fraudcheck?loanAmount=9999'
+    }
+    response {
+        status 200
+        body("""{
+    "fraudCheckStatus": "Not-Fraud",
+    "rejectionReason": "Amount OK"
+  }""")
+        headers {
+            header('Content-Type': 'text/plain;charset=ISO-8859-1')
+        }
+    }
 }
+]
